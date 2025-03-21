49c020649d35ffbea31f4c3888709a9d7b4b9ab8
DaniilGurski <daniil.gurski@elev.ga.ntig.se>
2025-03-21 22:08:46 +0100

    solution.

---
+++ src/script.js
@@ -0,0 +1,85 @@
+// Elements
+const descriptionField = document.querySelector("#desc");
+const amountField = document.querySelector("#amount");
+const incomeBtn = document.querySelector("#incomeBtn");
+const expenseBtn = document.querySelector("#expenseBtn");
+const incomeListElement = document.querySelector("#incomeList");
+const expenseListElement = document.querySelector("#expenseList");
+const transactionListElement = document.querySelector("#transactionList");
+const balanceElement = document.querySelector("#balance");
+const incomeList = [];
+const expenseList = [];
+const transactionList = [];
+let balance = 0;
+
+function emptyInputFields() {
+  amountField.value = "";
+  descriptionField.value = "";
+}
+
+function addTransaction(description, amount, type) {
+  transactionList.push({ description, amount, type });
+
+  // Update HTML transaction list
+  transactionListElement.innerHTML = "";
+  transactionList.forEach((transaction) => {
+    const transactionItem = document.createElement("li");
+    transactionItem.innerText = `${transaction.description} - ${transaction.amount} kr (${type})`;
+    transactionListElement.append(transactionItem);
+  });
+}
+
+// Handle income button click
+incomeBtn.addEventListener("click", () => {
+  const amount = Number(amountField.value);
+  const description = descriptionField.value;
+
+  if (amount === "" || description === "") {
+    return;
+  }
+
+  emptyInputFields();
+
+  // Update income list and balance
+  incomeList.push(amount);
+  balance += amount;
+  balanceElement.innerText = balance;
+
+  // Update HTML income list
+
+  incomeListElement.innerHTML = "";
+  incomeList.forEach((income) => {
+    const incomeItem = document.createElement("li");
+    incomeItem.innerText = `${description} - ${income} kr`;
+    incomeListElement.append(incomeItem);
+  });
+
+  addTransaction(description, amount, "Inkomst");
+});
+
+// Handle expense button click
+expenseBtn.addEventListener("click", () => {
+  const amount = Number(amountField.value);
+  const description = descriptionField.value;
+
+  if (amount === "" || description === "") {
+    return;
+  }
+
+  emptyInputFields();
+
+  // Update income list and balance
+  expenseList.push(amount);
+  balance -= amount;
+  balanceElement.innerHTML = balance;
+
+  // Update HTML expense list
+  expenseListElement.innerHTML = "";
+  expenseList.forEach((expense) => {
+    const expenseItem = document.createElement("li");
+    expenseItem.innerText = `${description} - ${expense} kr`;
+    expenseListElement.append(expenseItem);
+  });
+
+  addTransaction(description, amount, "Utgift");
+});
